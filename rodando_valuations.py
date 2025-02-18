1caf0b91aa424e39a2104c8d0011d5caa6710de3
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2025-02-18 13:35:46 -0300

    Adcionando github workflow

--- rodando_valuations.py
+++ rodando_valuations.py
@@ -10,7 +10,16 @@ data_valuation = {
     'preco_gordon': [],
     'preco_fluxo': [],
     'diferenca_gordon': [],
-    'diferenca_fluxo': []
+    'diferenca_fluxo': [],
+    'margem_ebit': [],
+    'variacao_receita': [],
+    'wacc': [],
+    'quantidade_acoes': [],
+    'divida_total': [],
+    'caixa': [],
+    'outros_ativos': [],
+    'passivos_menos_divida': [],
+    'percentual_imposto': [],
 }
 for acao in acoes:
     print('-'*10, acao, '-'*10)
@@ -50,6 +59,15 @@ for acao in acoes:
         data_valuation['preco_fluxo'].append(valor_fluxo['valor_por_acao'])
         data_valuation['diferenca_gordon'].append(round((preco_gordon['valuation_acao'] - valor_acao_atual)/valor_acao_atual,2))
         data_valuation['diferenca_fluxo'].append(round((valor_fluxo['valor_por_acao'] - valor_acao_atual)/valor_acao_atual,2))
+        data_valuation['margem_ebit'].append(indicadores['margemebit'])
+        data_valuation['variacao_receita'].append(indicadores['variacaoreceita'])
+        data_valuation['wacc'].append(indicadores['wacc'])
+        data_valuation['quantidade_acoes'].append(indicadores['quantidadeacoes'])
+        data_valuation['divida_total'].append(indicadores['dividatotal'])
+        data_valuation['caixa'].append(indicadores['caixa'])
+        data_valuation['outros_ativos'].append(indicadores['outrosativos'])
+        data_valuation['passivos_menos_divida'].append(indicadores['passivosmenosdivida'])
+        data_valuation['percentual_imposto'].append(indicadores['percentualimposto'])
     except:
         print(f'Erro ao obter dados da acao {acao}')
 data_valores = pd.DataFrame(data_valuation)
