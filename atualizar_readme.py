881c1f0ca1f244936df2f36e5d3240d4341e6e4e
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2025-02-19 15:05:50 -0300

    Adcionando remocao no arquivo markdow no python

--- atualizar_readme.py
+++ atualizar_readme.py
@@ -11,20 +11,17 @@ with open('README.md', 'r') as file:
     readme_content = file.readlines()
 
 # Verifica e localiza a seção "Resultados das Estimativas"
+section_found = False
 for i, line in enumerate(readme_content):
     if '## Estrutura do RepositÃ³rio' in line:
         index_fim = i
     if '## Resultados das Estimativas' in line:
         index_inicio = i
+
+readme_content = readme_content[:index_inicio+1] + readme_content[index_fim-1:]
         
-if len(readme_content[index_inicio:index_fim-1]) <= 10:
-    print("Tabela não existe ou foi removida.")
-    readme_content.insert(index_inicio + 1, f"### Tabela de Valuation\n{markdown_table}\n")
-else:
-    print("Tabela já existe, substituindo...")
-    readme_content[index_inicio+2:index_fim-1] = markdown_table.splitlines()
-    
-# Escreve a versão atualizada no README.md
+readme_content.insert(index_inicio + 1, f"### Tabela de Valuation\n{markdown_table}\n")
+
 with open('README.md', 'w') as file:
     file.writelines(readme_content)  
 
