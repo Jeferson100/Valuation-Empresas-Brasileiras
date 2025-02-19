71de2d8d80eee9668c4a2cdd92b5fa62d7af8e9a
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2025-02-19 13:42:23 -0300

    Adcionando tabela no readme

--- atualizar_readme.py
+++ atualizar_readme.py
@@ -11,26 +11,22 @@ with open('README.md', 'r') as file:
     readme_content = file.readlines()
 
 # Verifica e localiza a seção "Resultados das Estimativas"
-section_found = False
 for i, line in enumerate(readme_content):
+    if '## Estrutura do RepositÃ³rio' in line:
+        index_fim = i
     if '## Resultados das Estimativas' in line:
-        section_found = True
-        # Verifica se já existe uma tabela após essa seção
-        if any('Tabela de Valuation' in content for content in readme_content[i:]):
-            # Remove a tabela existente (caso já haja)
-            start_index = next(j for j, content in enumerate(readme_content[i:]) if 'Tabela de Valuation' in content) + i
-            # Remove a tabela existente (2 linhas: título + a própria tabela)
-            readme_content[start_index:start_index + 2] = []
-            print("Tabela existente removida.")
-        # Adiciona a nova tabela
-        readme_content.insert(i + 1, f"### Tabela de Valuation\n{markdown_table}\n")
-        break
-
-if not section_found:
-    print("Seção 'Resultados das Estimativas' não encontrada.")
-
+        index_inicio = i
+        
+if len(readme_content[index_inicio:index_fim-1]) <= 10:
+    print("Tabela não existe ou foi removida.")
+    readme_content.insert(index_inicio + 1, f"### Tabela de Valuation\n{markdown_table}\n")
+else:
+    print("Tabela já existe, substituindo...")
+    readme_content[index_inicio+2:index_fim-1] = markdown_table.splitlines()
+    
 # Escreve a versão atualizada no README.md
 with open('README.md', 'w') as file:
-    file.writelines(readme_content)
+    file.writelines(readme_content)  
+
 
 
